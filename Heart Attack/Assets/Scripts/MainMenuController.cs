9f1f5d1c1bde0f43250ebc0eca7fb805e72bbcb8
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2017-01-20 00:02:21 -0500

    Updated Main Menu buttons and completed basic functionality with scene loading where necessary and added Credits scene

---
+++ Heart Attack/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	// Load a scene from the main menu based on its name as an input
+	public void LoadScene (string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
